fb5de3ac8d1505405a34f1370b22b676220aca3c
Miguel Pacheco <angelpachecogranillo@gmail.com>
2023-11-13 20:44:26 -0600

    tailwindcss is added to the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
