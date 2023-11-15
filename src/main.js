89a16e9f287167829fb9748f77b3c8c8b75ec85b
Miguel Pacheco <angelpachecogranillo@gmail.com>
2023-11-15 15:29:06 -0600

    blog section for large devices

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ function getPost() {
             let html = ''
             json.forEach(({id, title, body}) => {
                 html += `
-                    <div id="post_card${id}" class="bg-primaryRed px-8 py-5 rounded-md">
+                    <div id="post_card${id}" class="bg-primaryRed px-8 py-5 rounded-md md:flex md:justify-between md:items-start md:content-center md:flex-col md:px-10 md:py-7">
                         <h5 class="text-white font-bold font-DM_Sans text-2xl mb-5">${title}</h5>
                         <p class="text-white font-light text-base leading-6 font-DM_Sans mb-5">${body}</p>
                         <button class="text-white font-bold text-base leading-6 font-DM_Sans">Learn more</button>
