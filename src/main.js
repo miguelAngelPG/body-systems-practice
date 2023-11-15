0b115c6ff193836dff04c7a3f75bb54cbea4e8db
Miguel Pacheco <angelpachecogranillo@gmail.com>
2023-11-15 10:23:10 -0600

    mobile percentage section

--- src/main.js
+++ src/main.js
@@ -18,13 +18,13 @@ function getPost() {
     fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3')
         .then(response => response.json())
         .then(json => {
-            
+
             let html = ''
             json.forEach(({id, title, body}) => {
                 html += `
                     <div id="post_card${id}" class="bg-primaryRed px-8 py-5 rounded-md">
                         <h5 class="text-white font-bold font-DM_Sans text-2xl mb-5">${title}</h5>
-                        <p class="text-white font-normal text-base leading-6 font-DM_Sans mb-5">${body}</p>
+                        <p class="text-white font-light text-base leading-6 font-DM_Sans mb-5">${body}</p>
                         <button class="text-white font-bold text-base leading-6 font-DM_Sans">Learn more</button>
                     </div>
                 `
